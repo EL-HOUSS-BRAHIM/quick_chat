6e74fd76894b44d6fbe434b4e4037877f514d9d6
EL-HOUSS-BRAHIM <brahim.crafts.tech@gmail.com>
2025-06-18 15:28:19 +0100

    pushing Message with the latest updates and fixes and deleted files

--- classes/Message.php
+++ classes/Message.php
@@ -393,7 +393,7 @@ class Message {
     
     private function isValidEmoji($emoji) {
         // Basic emoji validation - in production, use a more comprehensive check
-        return preg_match('/^(?:[\x{1F600}-\x{1F64F}]|[\x{1F300}-\x{1F5FF}]|[\x{1F680}-\x{1F6FF}]|[\x{1F1E0]-\x{1F1FF}]|[\x{2600}-\x{26FF}]|[\x{2700}-\x{27BF}])+$/u', $emoji);
+        return preg_match('/^(?:[\x{1F600}-\x{1F64F}]|[\x{1F300}-\x{1F5FF}]|[\x{1F680}-\x{1F6FF}]|[\x{1F1E0}-\x{1F1FF}]|[\x{2600}-\x{26FF}]|[\x{2700}-\x{27BF}])+$/u', $emoji);
     }
     
     private function getFileUrl($filePath) {
